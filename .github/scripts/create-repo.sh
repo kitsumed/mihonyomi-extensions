77288e9d36061f482352d1d9e5c813a9783332c6
Med <45147847+kitsumed@users.noreply.github.com>
2024-02-02 21:11:02 -0500

    Fix (#15)

--- .github/scripts/create-repo.sh
+++ .github/scripts/create-repo.sh
@@ -62,9 +62,12 @@ jq -c '.' < index.json > index.min.json
 #TMP PRINT OUT TREE IN CONSOLE FOR DEBUGGING (WELP)
 tree ./
 echo --------
-ls /releases/
+ls /home/runner/
 echo --------
-ls /main/
+ls ../releases
+echo --------
+echo --------
+ls ../main
 
 # Create repo.json file from releases-config.json
 #cp -f ../main/releases-config.json ./repo.json 
