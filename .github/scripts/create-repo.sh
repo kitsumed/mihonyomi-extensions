ad417c0db0ca7c1ef8cf500b1027403606e90d35
kitsumed <45147847+kitsumed@users.noreply.github.com>
2024-02-02 20:34:52 -0500

    [Debug] p2
    NOTE : max 1 parent in the path

--- .github/scripts/create-repo.sh
+++ .github/scripts/create-repo.sh
@@ -60,9 +60,11 @@ done | jq -sr '[.[]]' > index.json
 jq -c '.' < index.json > index.min.json
 
 #TMP PRINT OUT TREE IN CONSOLE FOR DEBUGGING (WELP)
-tree -d .../
+tree -d ./
+echo --------
+tree -d ../main/
 
 # Create repo.json file from releases-config.json
-#cp -f .../main/releases-config.json ./repo.json 
+#cp -f ../main/releases-config.json ./repo.json 
 
 cat index.json
